86398a7e5e4a96235d1cde81c373871772e950bc
yijun <yijunlee23@gmail.com>
2019-02-21 13:53:38 +0800

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  post 'logout', to: 'sessions#destroy', as: 'logout'
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
 

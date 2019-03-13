c2db175456b465ff53a3876fb6d769ecb8e3433c
yijun <yijunlee23@gmail.com>
2019-03-13 21:45:00 +0800

    user model modify

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,8 +30,7 @@ class SessionsController < ApplicationController
     else
       user = User.create_with_auth_and_hash(authentication, auth_hash)
       # you are expected to have a path that leads to a page for editing user details
-      @next = edit_user_path(user)
-      @notice = "User created. Please confirm or edit details"
+      @next = root_url
     end
   
     sign_in(user)

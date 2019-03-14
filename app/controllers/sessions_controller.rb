0f9dd3cee98768367d20c76f129c525de8e71080
yijun <yijunlee23@gmail.com>
2019-03-14 10:06:12 +0800

    fixed login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
@@ -11,6 +12,7 @@ class SessionsController < ApplicationController
       render "new"
     end
   end
+  
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
